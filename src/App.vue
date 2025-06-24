025b976e415a52a33228d3a306c25361b607fe6d
mu phaw <muphaw755@gmail.com>
2025-06-24 16:21:11 +0630

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+  <Button>Button click me</Button>
+  <h2 class="text-4xl text-red-300 text-center">Hello world </h2>
+</template>
+
